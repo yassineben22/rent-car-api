a8a129818754b5dc17ea638868c5a53429c5f850
Yassine Benlahbib <yassinebnlhbb@gmail.com>
2024-05-22 22:38:52 +0100

    final readme.md file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,6 +14,9 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         
+        //log username and password in console
+        // dd($request->all());
+        
         $validator = Validator::make($request->all(), [
             'username' => 'required|string',
             'password' => 'required|string',
